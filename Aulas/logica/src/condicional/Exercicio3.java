3c4e34a6f3e6d27a0540060c53e3dd1f3d76f3a6
TiagoTNX <tiagotnx@gmail.com>
2022-06-03 17:09:13 -0300

    Exercícios da aula 2

---
+++ Aulas/logica/src/condicional/Exercicio3.java
@@ -0,0 +1,45 @@
+package condicional;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+
+		int diaSemana;
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Insira um número inteiro de 1 a 7: ");
+		diaSemana = sc.nextInt();
+
+		switch (diaSemana) {
+		case 1:
+			System.out.println("O número 1 corresponde ao domingo.");
+			break;
+		case 2:
+			System.out.println("O número 2 corresponde a segunda-feira.");
+			break;
+		case 3:
+			System.out.println("O número 3 corresponde a terça-feira.");
+			break;
+		case 4:
+			System.out.println("O número 4 corresponde a quarta-feira.");
+			break;
+		case 5:
+			System.out.println("O número 5 corresponde a quinta-feira.");
+			break;
+		case 6:
+			System.out.println("O número 6 corresponde a sexta-feira.");
+			break;
+		case 7:
+			System.out.println("O número 7 corresponde ao sábado.");
+			break;
+		default:
+			System.out.println("O número informado é inválido.");
+		}
+
+		sc.close();
+
+	}
+
+}
